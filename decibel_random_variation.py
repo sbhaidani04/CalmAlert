de60eb5904ee2271671c86c609be3ab2e589f85b
Grace Padberg <gpadberg@ualberta.ca>
2024-11-02 12:48:34 -0600

    changed logic

--- decibel_random_variation.py
+++ decibel_random_variation.py
@@ -7,6 +7,17 @@ def decibel_variation(cur_value):
     return random_number
        
 
-print(decibel_variation(30))
+# print(decibel_variation(30))
 
+def stressed_decibel_variation(cur_stressed_value):
+    u_bound = cur_stressed_value + 4
+    l_bound = cur_stressed_value - 1
+    next_db = random.randint(l_bound,u_bound)
+    return next_db
 
+# # Testing function: 
+# db = stressed_decibel_variation(30)
+# for i in range(10):
+#     db = stressed_decibel_variation(db)
+#     print(db)
+ 
