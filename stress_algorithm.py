62d4adcddc66dffc7336715bd0e1abd07a120d8f
Grace Padberg <gpadberg@ualberta.ca>
2024-11-02 12:27:02 -0600

    fixed stress calculation logic

--- stress_algorithm.py
+++ stress_algorithm.py
@@ -7,35 +7,32 @@ import math
 # stress levels are from 0 to 100
 def hr_stress_level(user_avg_hr, user_cur_hr, k = 0.02):
     hr_diff = user_cur_hr - user_avg_hr
-    decibel_stress = 50 * (1 - math.exp(-k * (hr_diff)))
-    return decibel_stress
+    hr_stress = 50 * (1 - math.exp(-k * (hr_diff)))
+    return hr_stress
 
 def db_stress_level(decibel_value, threshold = 70, k = 0.1):
     if (decibel_value > threshold):
         # determine and add the stress level contribution by decibel level
         decibel_stress = 50 * (1 - math.exp(-k * (decibel_value - threshold)))
+    else:
+        decibel_stress = 0
     return decibel_stress
 
-def determine_stress_level(user_avg_hr, user_cur_hr, decibel_value, threshold = 70, k = 0.1):
-    stress_level = 0
-    hr_diff = user_cur_hr - user_avg_hr
-    if (hr_diff > 20):
-        # add the stress level contribution by hr
-        stress_level += hr_diff
-
-    if (decibel_value > threshold):
-        # determine and add the stress level contribution by decibel level
-        decibel_stress = 50 * (1 - math.exp(-k * (decibel_value - threshold)))
-        stress_level += decibel_stress
-    
+def overall_stress_level(user_average_hr, user_current_hr, db_value, threshold = 70):
+    hr_stress = hr_stress_level(user_average_hr, user_current_hr)
+    db_stress = db_stress_level(db_value)
+    stress_level = hr_stress+db_stress
     return stress_level
 
-# Test hr stress level:
-print(hr_stress_level(60, 120))
+# # Test hr stress level:
+# print(hr_stress_level(60, 120))
 
-# Test db stress level:
-print(db_stress_level(80))
+# # Test db stress level:
+# print(db_stress_level(80))
 
-# Test with various decibel levels
-for dB in [70, 75, 80, 85, 90, 95, 100, 120]:
-    print(f"Decibels: {dB} -> Stress: {determine_stress_level(10, 10, dB):.2f}")
+# # Test overall
+# print(overall_stress_level(60, 120, 80))
+
+# # Test with various decibel levels
+# for dB in [70, 75, 80, 85, 90, 95, 100, 120]:
+#     print(f"Decibels: {dB} -> Stress: {overall_stress_level(10, 10, dB):.2f}")
