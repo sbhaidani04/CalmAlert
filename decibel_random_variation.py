f8e5186f05c0f9a2f11959447fdb084b4c01ba11
Grace Padberg <gpadberg@ualberta.ca>
2024-11-02 12:50:59 -0600

    added threshold handling

--- decibel_random_variation.py
+++ decibel_random_variation.py
@@ -9,9 +9,13 @@ def decibel_variation(cur_value):
 
 # print(decibel_variation(30))
 
-def stressed_decibel_variation(cur_stressed_value):
-    u_bound = cur_stressed_value + 4
-    l_bound = cur_stressed_value - 1
+def stressed_decibel_variation(cur_stressed_value, threshold = 130):
+    if (cur_stressed_value < threshold):
+        u_bound = cur_stressed_value + 4
+        l_bound = cur_stressed_value - 1
+    else:
+        u_bound = cur_stressed_value + 1
+        l_bound = cur_stressed_value - 1
     next_db = random.randint(l_bound,u_bound)
     return next_db
 
