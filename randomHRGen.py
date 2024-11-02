508b59ff7ff1730617abd5f903925ef3634ed310
sbhaidani04 <sbhaidani04@gmail.com>
2024-11-02 13:49:35 -0600

    randomHRGen complete

--- randomHRGen.py
+++ randomHRGen.py
@@ -1,31 +1,44 @@
 import random
 import time
 
-# healthy heart rate (60 - 100 bpm)
+# stress values: 0 - 100
 
+# healthy heart rate (60 - 100 bpm)
 normalHRMin = 60
 normalHRMax = 100
+normalHRAvg = int((normalHRMin + normalHRMax) / 2) # average of the 2 min and max
 
-stressHRMin = 101
-stressHRMax = 200
+stressHRMin = 150
+stressHRMax = 220
 
-normalHRs = []
-stressHRs = []
 
-count = 5
-
-def randomHRGenerator():
-
-    for i in range (count):
-        normalHRs.append(random.randint(normalHRMin, normalHRMax))
-        time.sleep(3)
-
-    for i in range (count):
-        stressHRs.append(random.randint(stressHRMin, stressHRMax))
-        time.sleep(3)
-
-    return normalHRs, stressHRs
-
-print(randomHRGenerator())
+# simulating normal heart rate by generating a random integer between the theoretical min and max HRs
+# parameters: normalHRMin, normalHRMax
+def normalHRGenerator(pastVal):
+    
+    if (pastVal == normalHRMin | pastVal == (normalHRMin + 3) | pastVal == (normalHRMin - 3)): # if pastVal equals the min, then generate value slightly higher than min but <= avg
+        normalHR = random.randint(normalHRMin + 3, normalHRAvg)
+    elif (pastVal == normalHRAvg): # if pastVAL equal to the peak, then generate value slightly higher than avg but <= max 
+        normalHR = random.randint(normalHRAvg + 3, normalHRMax)
+    elif (pastVal == normalHRMax | pastVal == (normalHRMax + 3) | pastVal == (normalHRMax - 3)): # if pastVal approx. equals the max, then generate value slightly lower than max but <= avg
+        normalHR = random.randint(normalHRAvg, normalHRMax -3)
+    elif (normalHRMin < pastVal < normalHRAvg): # if pastVal bless than peak, then generate value slightly higher than pastVal but <= avg
+        if (pastVal + 3 <= normalHRAvg):
+            normalHR = random.randint(pastVal + 3, normalHRAvg)
+        else:
+            normalHR = random.randint(normalHRAvg + 3, normalHRMax)
+    elif (normalHRAvg < pastVal < normalHRMax): # if pastVal greater than peak, then generate value slightly higher than pastVal but <= max
+        if (pastVal + 3 <= normalHRMax):
+            normalHR = random.randint(pastVal + 3, normalHRMax)
+        else:
+            normalHR = random.randint(normalHRAvg, pastVal)
+    else:
+        normalHR = random.randint(normalHRMin, normalHRMax)    
+    return normalHR
+       
+normalHR = random.randint(normalHRMin, normalHRMax)
+print(normalHR)
+temp = normalHRGenerator(normalHR)
+print (temp)
 
 
